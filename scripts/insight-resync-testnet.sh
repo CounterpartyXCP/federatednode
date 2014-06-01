411b3b0ed3fa308550a7926155bdea3c26dca083
xnova <xnova@counterparty.co>
2014-06-01 13:48:46 +0000

    mod insight resync scripts

--- scripts/insight-resync-testnet.sh
+++ scripts/insight-resync-testnet.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CURDIR=`pwd`
 cd ~xcp/insight-api
-export BITCOIND_DATADIR=~xcp/.bitcoin-testnet/
-export BITCOIND_USER=`cat ~xcp/.bitcoin-testnet/bitcoin.conf | sed -n 's/.*rpcuser=\([^ \n]*\).*/\1/p'`
-export BITCOIND_PASS=`cat ~xcp/.bitcoin-testnet/bitcoin.conf | sed -n 's/.*rpcpassword=\([^ \n]*\).*/\1/p'`
-INSIGHT_NETWORK=testnet INSIGHT_DB=~xcp/insight-api/db util/sync.js -D
+export BITCOIND_DATADIR=/home/xcp/.bitcoin-testnet/
+export BITCOIND_USER=`cat /home/xcp/.bitcoin-testnet/bitcoin.conf | sed -n 's/.*rpcuser=\([^ \n]*\).*/\1/p'`
+export BITCOIND_PASS=`cat /home/xcp/.bitcoin-testnet/bitcoin.conf | sed -n 's/.*rpcpassword=\([^ \n]*\).*/\1/p'`
+INSIGHT_NETWORK=testnet INSIGHT_DB=/home/xcp/insight-api/db util/sync.js -D
 cd $CURDIR
--- scripts/insight-resync.sh
+++ scripts/insight-resync.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CURDIR=`pwd`
-cd ~xcp/insight-api
-export BITCOIND_DATADIR=~xcp/.bitcoin/
-export BITCOIND_USER=`cat ~xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcuser=\([^ \n]*\).*/\1/p'`
-export BITCOIND_PASS=`cat ~xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcpassword=\([^ \n]*\).*/\1/p'`
-INSIGHT_NETWORK=livenet INSIGHT_DB=~xcp/insight-api/db util/sync.js -D
+cd /home/xcp/insight-api
+export BITCOIND_DATADIR=/home/xcp/.bitcoin/
+export BITCOIND_USER=`cat /home/xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcuser=\([^ \n]*\).*/\1/p'`
+export BITCOIND_PASS=`cat /home/xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcpassword=\([^ \n]*\).*/\1/p'`
+INSIGHT_NETWORK=livenet INSIGHT_DB=/home/xcp/insight-api/db util/sync.js -D
 cd $CURDIR
