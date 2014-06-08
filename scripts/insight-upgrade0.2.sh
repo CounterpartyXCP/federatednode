c55dd51da22bd924d1ebe33305dfc0a369e9a531
xnova <xnova@counterparty.co>
2014-06-08 14:39:14 +0000

    remove insight upgrade scripts, which didn't really work

--- scripts/insight-upgrade0.2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CURDIR=`pwd`
-cd /home/xcp/insight-api
-export BITCOIND_DATADIR=/home/xcp/.bitcoin/
-export BITCOIND_USER=`cat /home/xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcuser=\([^ \n]*\).*/\1/p'`
-export BITCOIND_PASS=`cat /home/xcp/.bitcoin/bitcoin.conf | sed -n 's/.*rpcpassword=\([^ \n]*\).*/\1/p'`
-INSIGHT_NETWORK=livenet INSIGHT_DB=/home/xcp/insight-api/db util/upgradeV0.2js
-cd $CURDIR
