99280f06c202b788143757ec240a3e63eac36030
Robby Dermody <xnova@counterparty.co>
2014-11-08 20:50:46 -0700

    more run.py cleanup

--- run.py
+++ run.py
@@ -13,19 +13,19 @@ if os.name == "posix" and os.geteuid() == 0:
     print("Please run this script as a non-root user.")
     sys.exit(1)
 
-run_tests = False
-run_counterblockd = False
-run_armory_utxsvr = False
-args = sys.argv[1:]
+run = None
 if len(sys.argv) >= 2 and sys.argv[1] == 'tests':
-    run_tests = True
     args = sys.argv[2:]
+    run = 'tests'
 elif len(sys.argv) >= 2 and sys.argv[1] == 'counterblockd':
-    run_counterblockd = True
     args = sys.argv[2:]
+    run = 'counterblockd'
 elif len(sys.argv) >= 2 and sys.argv[1] == 'armory_utxsvr':
-    run_armory_utxsvr = True
     args = sys.argv[2:]
+    run = 'armory_utxsvr'
+else:
+    args = sys.argv[1:]
+    run = 'counterpartyd'
     
 base_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 env_path = os.path.join(base_path, "env")
@@ -43,22 +43,23 @@ def delete_lockfile(component):
         print("Removing stale lock file at '%s'" % p)
         os.remove(p)
 
-if run_tests:
+if run == 'tests':
     pytest_path = os.path.join(env_path, "Scripts" if os.name == "nt" else "bin", "py.test.exe" if os.name == "nt" else "py.test")
     counterpartyd_tests_path = os.path.join(dist_path, "counterpartyd", "test", "test_.py")
     command = "%s %s %s" % (pytest_path, counterpartyd_tests_path, ' '.join(args))
-elif run_counterblockd:
+elif run == 'counterblockd':
     delete_lockfile("counterblockd")
     counterblockd_env_path = os.path.join(base_path, "env.counterblockd")
     counterblockd_python_path = os.path.join(counterblockd_env_path, "Scripts" if os.name == "nt" else "bin", "python.exe" if os.name == "nt" else "python")
     counterblockd_path = os.path.join(dist_path, "counterblockd", "counterblockd.py")
     command = "%s %s %s" % (counterblockd_python_path, counterblockd_path, ' '.join(args))
-elif run_armory_utxsvr:
+elif run == 'armory_utxsvr':
     armory_utxsvr_env_path = os.path.join(base_path, "env.counterblockd") #use the counterblock venv
     armory_utxsvr_python_path = os.path.join(armory_utxsvr_env_path, "Scripts" if os.name == "nt" else "bin", "python.exe" if os.name == "nt" else "python")
     armory_utxsvr_path = os.path.join(dist_path, "counterblockd", "armory_utxsvr.py")
     command = "DISPLAY=localhost:1.0 xvfb-run --auto-servernum %s %s %s" % (armory_utxsvr_python_path, armory_utxsvr_path, ' '.join(args))
-else: #run counterpartyd
+else:
+    assert run == 'counterpartyd'
     delete_lockfile("counterpartyd")
     counterpartyd_path = os.path.join(dist_path, "counterpartyd", "counterpartyd.py")
     command = "%s %s %s" % (python_path, counterpartyd_path, ' '.join(args))
