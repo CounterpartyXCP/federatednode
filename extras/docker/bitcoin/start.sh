50c27609f047632b9a56a9a2ca56269b39f3b791
robby-dermody <robbyd@u20.org>
2016-06-23 15:55:54 -0400

    additional changes

--- extras/docker/bitcoin/start.sh
+++ extras/docker/bitcoin/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 : ${PARAMS:=""}
-/usr/local/bin/bitcoind ${PARAMS} "$@"
+
+# Respond to docker stop's SIGTERM gracefully
+stopcmd="/usr/local/bin/bitcoin-cli ${PARAMS} $@ stop; exit 0"
+trap "$stopcmd" SIGTERM
+
+/usr/local/bin/bitcoind ${PARAMS} $@
