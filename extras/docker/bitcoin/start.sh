3af0f3114e4e9705cdc8d93d03d2161bb07db3f4
robby-dermody <robbyd@u20.org>
2016-06-26 10:47:51 -0400

    handle sigterm/sigint properly in containers (no need for graceful_shutdown kludge). also, simplify 'reparse'

--- extras/docker/bitcoin/start.sh
+++ extras/docker/bitcoin/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-: ${PARAMS:=""}
-
 # Respond to docker stop's SIGTERM gracefully
-stopcmd="/usr/local/bin/bitcoin-cli ${PARAMS} stop; exit 0"
-trap "$stopcmd" SIGINT SIGTERM
-
-/usr/local/bin/bitcoind ${PARAMS} $@
+# Utilize the SIGTERM propagation pattern from http://veithen.github.io/2014/11/16/sigterm-propagation.html
+: ${PARAMS:=""}
+trap 'kill -TERM $PID' TERM INT
+/usr/local/bin/bitcoind ${PARAMS} $@ &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
--- fednode.py
+++ fednode.py
@@ -148,15 +148,6 @@ def is_container_running(service, abort_on_not_exist=True):
     return container_running
 
 
-def graceful_service_shutdown(services):
-    """bitcoind must be properly stopped on windows docker otherwise the blocks we downloaded don't seem to persist. this hack helps ensure that....
-    for other OSes, it allows more graceful shutdown of bitcoind"""
-    if (not services or 'bitcoin' in services) and is_container_running('bitcoin'):
-        os.system('{} docker exec -i -t federatednode_bitcoin_1 bash -c "bitcoin-cli stop &>/dev/null"'.format(SUDO_CMD))
-    if (not services or 'bitcoin-testnet' in services) and is_container_running('bitcoin-testnet'):
-        os.system('{} docker exec -i -t federatednode_bitcoin-testnet_1 bash -c "bitcoin-cli -conf=/root/.bitcoin-config/bitcoin.testnet.conf stop &>/dev/null"'.format(SUDO_CMD))
-
-
 def main():
     global DOCKER_CONFIG_PATH
     setup_env()
@@ -246,17 +237,12 @@ def main():
     elif args.command == 'start':
         run_compose_cmd("start {}".format(' '.join(args.services)))
     elif args.command == 'stop':
-        graceful_service_shutdown(args.services)
         run_compose_cmd("stop {}".format(' '.join(args.services)))
     elif args.command == 'restart':
-        graceful_service_shutdown(args.services)
         run_compose_cmd("restart {}".format(' '.join(args.services)))
     elif args.command == 'reparse':
         run_compose_cmd("stop {}".format(args.service))
-        if args.service in ['counterparty', 'counterparty-testnet']:
-            run_compose_cmd("run -e COMMAND=reparse {}".format(args.service))
-        elif args.service in ['counterblock', 'counterblock-testnet']:
-            run_compose_cmd("run -e EXTRA_PARAMS=\"--reparse\" {}".format(args.service))
+        run_compose_cmd("run -e COMMAND=reparse {}".format(args.service))
     elif args.command == 'tail':
         run_compose_cmd("logs -f --tail=50 {}".format(' '.join(args.services)))
     elif args.command == 'logs':
