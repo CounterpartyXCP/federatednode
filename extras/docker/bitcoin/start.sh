1b04238ae372b27d9eb2911fadceeae8abf02ac8
robby-dermody <robbyd@u20.org>
2016-06-26 10:52:42 -0400

    comment

--- extras/docker/bitcoin/start.sh
+++ extras/docker/bitcoin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Respond to docker stop's SIGTERM gracefully
-# Utilize the SIGTERM propagation pattern from http://veithen.github.io/2014/11/16/sigterm-propagation.html
+# Launch, utilizing the SIGTERM/SIGINT propagation pattern from
+# http://veithen.github.io/2014/11/16/sigterm-propagation.html
 : ${PARAMS:=""}
 trap 'kill -TERM $PID' TERM INT
 /usr/local/bin/bitcoind ${PARAMS} $@ &
